c740a2fde1668b4aec936a605a44d58a09442faf
sveltbold-sudo <sveltbold@gmail.com>
2026-03-16 14:44:09 +0100

    fix: enable unoptimized images for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 const nextConfig = {
   images: {
-    unoptimized: false,
+    unoptimized: true,
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
