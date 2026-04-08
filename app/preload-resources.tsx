e403f7bce554a41157d18bb152765edf7b95ca22
sveltbold-sudo <sveltbold@gmail.com>
2026-04-08 19:42:26 +0200

    🐛 Fix: Retirer préchargement police inexistante

--- app/preload-resources.tsx
+++ app/preload-resources.tsx
@@ -5,20 +5,6 @@ import { useEffect } from 'react'
 // Précharger les ressources critiques
 export function PreloadResources() {
   useEffect(() => {
-    // Précharger les polices
-    const fontLinks = [
-      '/fonts/inter-var.woff2',
-    ]
-    
-    fontLinks.forEach(href => {
-      const link = document.createElement('link')
-      link.rel = 'preload'
-      link.as = 'font'
-      link.href = href
-      link.crossOrigin = 'anonymous'
-      document.head.appendChild(link)
-    })
-
     // Préconnecter aux domaines externes
     const preconnectDomains = [
       'https://images.unsplash.com',
