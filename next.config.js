340fb7adcde247aaa9f4fda5999fd30d90963c5d
sveltbold-sudo <sveltbold@gmail.com>
2026-07-14 13:11:57 +0200

    fix: restore 'unsafe-inline' in script-src CSP (required by Next.js 16 hydration)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = [
   "default-src 'self'",
-  "script-src 'self' https://accounts.google.com/gsi/client",
+  "script-src 'self' 'unsafe-inline' https://accounts.google.com/gsi/client",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob: https:",
   "font-src 'self' https://fonts.gstatic.com",
