b4f8d1e795e93bda5b6f8f6f31525c1b90cbd8bb
sveltbold-sudo <sveltbold@gmail.com>
2026-07-24 18:39:56 +0200

    chore: remove temp scripts

--- scripts/check-categories.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-async function main() {
-  const cats = await prisma.category.findMany({ select: { slug: true, image: true } })
-  cats.forEach(c => console.log(c.slug + ': ' + c.image))
-  await prisma.$disconnect()
-}
-main()
